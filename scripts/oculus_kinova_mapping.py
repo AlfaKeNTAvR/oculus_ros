0541b254746e52992bb4c7ba932b2b82d6d077bc
Nikita Boguslavskii <bognik3@gmail.com>
2023-05-01 10:34:01 -0400

    refactor: Move compensations from positional_control

--- scripts/oculus_kinova_mapping.py
+++ scripts/oculus_kinova_mapping.py
@@ -4,9 +4,10 @@
 """
 
 import rospy
+import numpy as np
+import transformations
 
 from geometry_msgs.msg import (Pose)
-from std_msgs.msg import (Bool)
 
 from oculus.msg import (ControllerButtons)
 from kinova_positional_control.srv import (
@@ -48,7 +49,10 @@ class OculusKinovaMapping:
         self.TRACKING_MODE = tracking_mode
 
         # # Private variables:
-        self.__oculus_pose = Pose()
+        self.__oculus_pose = {
+            'position': np.array([0.0, 0.0, 0.0]),
+            'orientation': np.array([1.0, 0.0, 0.0, 0.0]),
+        }
         self.__oculus_buttons = ControllerButtons()
 
         self.__tracking_state_machine_state = 0
@@ -57,7 +61,23 @@ class OculusKinovaMapping:
         self.__control_mode = 'position'
 
         # # Public variables:
-        self.tracking = False
+        self.pose_tracking = False
+
+        # Last commanded Relaxed IK pose is required to compensate controller
+        # input.
+        self.last_relaxed_ik_pose = {
+            'position': np.array([0.0, 0.0, 0.0]),
+            'orientation': np.array([1.0, 0.0, 0.0, 0.0]),
+        }
+
+        # This difference is calculated each time the tracking is started and
+        # subracted from future inputs during current tracking to compensate for
+        # linear misalignment between the global and relaxed_ik coordinate
+        # systems.
+        self.oculus_relaxed_ik_difference = {
+            'position': np.array([0.0, 0.0, 0.0]),
+            'orientation': np.array([1.0, 0.0, 0.0, 0.0]),
+        }
 
         # # ROS node:
 
@@ -74,13 +94,8 @@ class OculusKinovaMapping:
         )
 
         # # Topic publisher:
-        self.__kinova_tracking = rospy.Publisher(
-            f'{self.ROBOT_NAME}/controller/tracking',
-            Bool,
-            queue_size=1,
-        )
         self.__kinova_pose = rospy.Publisher(
-            f'{self.ROBOT_NAME}/controller/pose',
+            f'{self.ROBOT_NAME}/input_pose',
             Pose,
             queue_size=1,
         )
@@ -96,6 +111,11 @@ class OculusKinovaMapping:
             ControllerButtons,
             self.__oculus_buttons_callback,
         )
+        rospy.Subscriber(
+            'relaxed_ik/commanded_pose_gcs',
+            Pose,
+            self.__commanded_pose_callback,
+        )
 
     # # Service handlers:
 
@@ -105,19 +125,14 @@ class OculusKinovaMapping:
 
         """
 
-        if self.__control_mode == 'full':
-            self.__oculus_pose = message
+        self.__oculus_pose['position'][0] = message.position.x
+        self.__oculus_pose['position'][1] = message.position.y
+        self.__oculus_pose['position'][2] = message.position.z
 
-        elif self.__control_mode == 'position':
-            self.__oculus_pose.position = message.position
-            # TODO: Proper orientation support.
-            self.__oculus_pose.orientation.w = 0.6532815
-            self.__oculus_pose.orientation.x = -0.2705981
-            self.__oculus_pose.orientation.y = -0.2705981
-            self.__oculus_pose.orientation.z = 0.6532815
-
-        elif self.__control_mode == 'orientation':
-            self.__oculus_pose.orientation = message.orientation
+        self.__oculus_pose['orientation'][0] = message.orientation.w
+        self.__oculus_pose['orientation'][1] = message.orientation.x
+        self.__oculus_pose['orientation'][2] = message.orientation.y
+        self.__oculus_pose['orientation'][3] = message.orientation.z
 
     def __oculus_buttons_callback(self, message):
         """
@@ -128,6 +143,20 @@ class OculusKinovaMapping:
 
         if self.TRACKING_MODE == 'hold':
             self.tracking = self.__oculus_buttons.grip_button
+
+    def __commanded_pose_callback(self, message):
+        """
+        
+        """
+
+        self.last_relaxed_ik_pose['position'][0] = message.position.x
+        self.last_relaxed_ik_pose['position'][1] = message.position.y
+        self.last_relaxed_ik_pose['position'][2] = message.position.z
+
+        self.last_relaxed_ik_pose['orientation'][0] = message.orientation.w
+        self.last_relaxed_ik_pose['orientation'][1] = message.orientation.x
+        self.last_relaxed_ik_pose['orientation'][2] = message.orientation.y
+        self.last_relaxed_ik_pose['orientation'][3] = message.orientation.z
 
     # # Private methods:
     def __tracking_state_machine(self, button):
@@ -141,17 +170,46 @@ class OculusKinovaMapping:
 
         # State 1: Grip button was released. Tracking is activated.
         elif (self.__tracking_state_machine_state == 1 and not button):
-            self.tracking = True
             self.__tracking_state_machine_state = 2
+
+            self.__calculate_compensation()
+            self.pose_tracking = True
 
         # State 2: Grip button was pressed. Tracking is deactivated.
         elif (self.__tracking_state_machine_state == 2 and button):
-            self.tracking = False
             self.__tracking_state_machine_state = 3
+
+            self.pose_tracking = False
 
         # State 3: Grip button was released.
         elif (self.__tracking_state_machine_state == 3 and not button):
             self.__tracking_state_machine_state = 0
+
+    def __mode_state_machine(self, button):
+        """
+        
+        """
+
+        # State 0: Button was pressed.
+        if (self.__mode_state_machine_state == 0 and button):
+            self.__mode_state_machine_state = 1
+
+            self.__control_mode = 'full'
+            self.__calculate_compensation()
+
+        # State 1: Button was released.
+        elif (self.__mode_state_machine_state == 1 and not button):
+            self.__mode_state_machine_state = 3
+
+        # State 2: Button was pressed.
+        elif (self.__mode_state_machine_state == 3 and button):
+            self.__mode_state_machine_state = 4
+
+            self.__control_mode = 'position'
+
+        # State 3: Button was released.
+        elif (self.__mode_state_machine_state == 4 and not button):
+            self.__mode_state_machine_state = 0
 
     def __gripper_state_machine(self, button):
         """
@@ -176,28 +234,23 @@ class OculusKinovaMapping:
         elif (self.__gripper_state_machine_state == 3 and not button):
             self.__gripper_state_machine_state = 0
 
-    def __mode_state_machine(self, button):
-        """
+    def __calculate_compensation(self):
+        """Calculates the compensation for coordinate systems misalignment.
         
         """
 
-        # State 0: Button was pressed.
-        if (self.__mode_state_machine_state == 0 and button):
-            self.__control_mode = 'position'
-            self.__mode_state_machine_state = 1
-
-        # State 1: Button was released.
-        elif (self.__mode_state_machine_state == 1 and not button):
-            self.__mode_state_machine_state = 3
-
-        # State 2: Button was pressed.
-        elif (self.__mode_state_machine_state == 3 and button):
-            self.__control_mode = 'full'
-            self.__mode_state_machine_state = 4
-
-        # State 3: Button was released.
-        elif (self.__mode_state_machine_state == 4 and not button):
-            self.__mode_state_machine_state = 0
+        self.oculus_relaxed_ik_difference['position'] = (
+            self.__oculus_pose['position']
+            - self.last_relaxed_ik_pose['position']
+        )
+        self.oculus_relaxed_ik_difference['orientation'] = (
+            transformations.quaternion_multiply(
+                self.last_relaxed_ik_pose['orientation'],
+                transformations.quaternion_inverse(
+                    self.__oculus_pose['orientation']
+                ),
+            )
+        )
 
     # # Public methods:
     def main_loop(self):
@@ -208,11 +261,52 @@ class OculusKinovaMapping:
         if self.TRACKING_MODE == 'press':
             self.__tracking_state_machine(self.__oculus_buttons.grip_button)
 
-        self.__kinova_tracking.publish(self.tracking)
-        self.__kinova_pose.publish(self.__oculus_pose)
+        if self.pose_tracking:
+            self.publish_kinova_pose()
 
         self.__gripper_state_machine(self.__oculus_buttons.trigger_button)
         self.__mode_state_machine(self.__oculus_buttons.primary_button)
+
+    def publish_kinova_pose(self):
+        """
+        
+        """
+
+        compensated_input_pose = {
+            'position': np.array([0.0, 0.0, 0.0]),
+            'orientation': np.array([1.0, 0.0, 0.0, 0.0]),
+        }
+
+        compensated_input_pose['position'] = (
+            self.__oculus_pose['position']
+            - self.oculus_relaxed_ik_difference['position']
+        )
+
+        # Use fixed orientation.
+        compensated_input_pose['orientation'] = (
+            self.last_relaxed_ik_pose['orientation']
+        )
+
+        # Use oculus orientation.
+        if self.__control_mode == 'full':
+            compensated_input_pose['orientation'] = (
+                transformations.quaternion_multiply(
+                    self.oculus_relaxed_ik_difference['orientation'],
+                    self.__oculus_pose['orientation'],
+                )
+            )
+
+        pose_message = Pose()
+        pose_message.position.x = compensated_input_pose['position'][0]
+        pose_message.position.y = compensated_input_pose['position'][1]
+        pose_message.position.z = compensated_input_pose['position'][2]
+
+        pose_message.orientation.w = compensated_input_pose['orientation'][0]
+        pose_message.orientation.x = compensated_input_pose['orientation'][1]
+        pose_message.orientation.y = compensated_input_pose['orientation'][2]
+        pose_message.orientation.z = compensated_input_pose['orientation'][3]
+
+        self.__kinova_pose.publish(pose_message)
 
 
 def node_shutdown():
